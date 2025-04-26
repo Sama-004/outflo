0c9980f7a27f2ebbd1eed3ddc90647b84b12097b
sama-004 <70210929+Sama-004@users.noreply.github.com>
2025-04-26 12:13:22 +0530

    init: backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
